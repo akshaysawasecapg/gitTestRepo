c4b09f0fbe5b01011547eedfe03f593f9991d37b
akshaysawasecapg <akshay.sawase@capgemini.com>
2023-01-17 11:41:48 +0530

    First commit to source code repo

---
+++ src/gitprojectTest/TestProj.java
@@ -0,0 +1,12 @@
+package gitprojectTest;
+
+public class TestProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("TEST GITHUB PROJECT !!!!");
+
+	}
+
+}
